bf00ee9296d5d0388fed0e3e7dddeac1eb2dcb33
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2022-11-23 23:05:57 +0000

    Hang

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+const leastFavoriteCustomer ="Elephant"
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'; 
+}
+
+function overwriteBestCustomer(val){
+    bestCustomer = val;
+}
+
+// const leastFavoriteCustomer = 'Mercy';
+
+// Write your solution in this file!
+var customerName = 'bob';
+
+// const leastFavoriteCustomer ="Elephant"
+
+// function upperCaseCustomerName() {
+//     customerName = customerName.toUpperCase();
+// }
+
+// function setBestCustomer(){
+//     bestCustomer = 'not bob'; 
+// }
+
+// function overwriteBestCustomer(val){
+//     bestCustomer = val;
+// }
+
+// const leastFavoriteCustomer = 'Mercy';
